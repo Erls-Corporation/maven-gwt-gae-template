878486a8c0fe4d904d6b728d09aed1a49cc0394f
Jonathan Verrecchia <verrecchia.j@.paris.sfeir.com>
2011-03-25 12:41:21 +0100

    Little changes

---
+++ src/test/java/com/jverrecchia/template/client/UserTest.java
@@ -0,0 +1,16 @@
+package com.jverrecchia.template.client;
+
+import com.jverrecchia.template.shared.User;
+
+import junit.framework.TestCase;
+
+
+public class UserTest extends TestCase{
+	
+	public void testUserName(){
+		User user = new User();
+		user.setName("Jonathan");
+		assert(user.getName().equals("Jonathan"));
+	}	
+	
+}
